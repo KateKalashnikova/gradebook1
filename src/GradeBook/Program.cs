9afef52f8b76205e951200d7341ebb0e7837f6ea
Kate Kalashnikova <kkalashnikova@kruxanalytics.com>
2021-06-10 14:13:47 -0600

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book();
+
+            var grades = new List<double>(){12.7, 10.3, 15.5, 44};
+            grades.Add(56.1);
+            book.AddGrade(89.5);            
+          
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+            result /= grades.Count; 
+            Console.WriteLine($"The average grade is {result:N1}");
+            
+        }
+    }
+}
